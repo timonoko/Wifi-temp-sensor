1950483e04aeb559b99f8cdf84deb237305063cf
Timo Noko <timonoko@users.noreply.github.com>
2021-09-07 11:55:31 +0300

    --

--- boot.py
+++ boot.py
@@ -4,7 +4,7 @@ def do_connect():
     if not sta_if.isconnected():
         print('connecting to network...')
         sta_if.active(True)
-        sta_if.connect('Jorpakko', 'Juhannusyona')
+        sta_if.connect('Jorpakko', 'Salasana')
         while not sta_if.isconnected():
             pass
     print('IF network config:', sta_if.ifconfig())
